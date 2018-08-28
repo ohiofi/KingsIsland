6bc6cfe4fbcb872f6219b7ff426f3646bfb4b8c1
Justin Riley <20hz20khz@gmail.com>
2018-08-28 09:49:06 -0400

    init

---
+++ KingsIsland1.java
@@ -0,0 +1,40 @@
+//import cs1.Keyboard;
+import java.util.Scanner;
+
+//You are traveling to King's Island for the day.
+//Everyone in the group has agreed to split the cost evenly.
+//Write a program to determine the total cost of the trip and the cost per person.
+//Assume that each ticket is $32.99
+
+
+public class KingsIsland1
+{
+	public static void main(String[] args)
+	{
+		Scanner myScanner = new Scanner(System.in);
+		//Declare variables for the number of people, parking cost, food cost, total cost, cost per person
+		int numPeople;
+		System.out.print("How many people?");
+		numPeople = myScanner.nextInt();
+		String username = myScanner.nextLine();
+
+		//Prompt for and read in the number of people in the group
+
+
+		//Prompt for and read in the cost of parking
+
+
+		//Prompt for and read in the amount spent on food
+
+
+		//Calculate the total expenses
+
+
+		//Calculate the average per person
+
+
+		//Display results
+
+
+	}
+}
